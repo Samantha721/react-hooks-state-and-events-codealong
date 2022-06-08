e8a4b4ba9bb65b7802e8c0050507c9a271afb7dc
Samantha Navarro <samantha.m.navarro@gmail.com>
2022-06-08 14:54:08 -0700

    commit tree 1a24e1b06cfadf1bed06686722f28bd292338c50

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,11 @@
 import React from "react";
 import Toggle from "./Toggle";
+// import React, { useState } from 'react';
 
 function App() {
+
+  // const [isOn, setIsOn] = useState(false);
+
   return (
     <div>
       <h3>Toggle</h3>
